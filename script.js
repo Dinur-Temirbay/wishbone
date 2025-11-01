3996800098433f75a1c4532d8882b93ce3032fdd
Dinur <DinurTemirbai@mail.ru>
2025-11-02 00:34:15 +0500

    burger menu works and minor fixes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+const menuLinks = document.querySelectorAll('.header__link');
+
+burger.addEventListener('click', () => {
+	burger.classList.toggle('active');
+	menu.classList.toggle('active');
+});
+
+menuLinks.forEach(link => {
+	link.addEventListener('click', () => {
+		burger.classList.remove('active');
+		menu.classList.remove('active');
+	});
+});
